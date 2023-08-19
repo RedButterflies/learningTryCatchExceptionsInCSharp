50d5bc6b8fe69b70151248be7a80610fdc7d40f3
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-20 00:13:47 +0200

    added a try catch statement

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -1,2 +1,17 @@
 ﻿//try and catch, exceptions C#
 
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main (string[] args)
+    {        try
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(numbers[11]);
+        }
+        catch (Exception e) { Console.WriteLine ("An error occured! Please try again! "+e.Message); }
+        finally { Console.WriteLine("The try catch has finished"); }
+    }
+}
